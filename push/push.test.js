95af1bf99f05dfc8ac48583db34e08fdb88530bc
mariayanguas <maria.yanguas@thekeytalent.com>
2022-05-09 19:00:16 +0200

    Fix text error

--- push/push.test.js
+++ push/push.test.js
@@ -1,7 +1,6 @@
-/* eslint-disable no-undef */
 import { push } from './push';
 
-describe('Give the function push', () => {
+describe('Given the function push', () => {
   describe('When recibe a new item', () => {
     test('should return the new length', () => {
       //Arrange
