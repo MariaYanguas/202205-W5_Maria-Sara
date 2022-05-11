d3d71e5c12bd476c96445cc65bb2077e4c9bccea
Sara Carreras <saracc621@gmail.com>
2022-05-11 19:48:20 +0200

    TypeErrors on function

--- shift/shift.js
+++ shift/shift.js
@@ -1,5 +1,14 @@
 //SHIFT
 export const shift = (arr) => {
+  if (typeof arr === []) {
+    throw new TypeError('Invalid array length');
+  }
+  if (typeof arr === 'string') {
+    throw new TypeError("Can't use shift on a string");
+  } else if (typeof arr === 'number') {
+    throw new TypeError("Can't use shift on a number");
+  }
+
   let firstItem = arr[0];
   for (let i = 0; i < arr.length; i++) {
     arr[i] = arr[i + 1];
