5867559551faef96eadf9211647bf767e0f2b94b
Sara Carreras <saracc621@gmail.com>
2022-05-11 17:53:46 +0200

    covering tests with NaN value

--- pop/pop.js
+++ pop/pop.js
@@ -6,7 +6,7 @@ export const pop = (arr) => {
     throw new TypeError("Can't use pop on a number");
   }
   for (let i = 0; i < arr.length; i++) {
-    newArray = [arr[i]];
+    newArray = arr[i];
   }
   arr.length = arr.length - 1;
 
--- pop/pop.test.js
+++ pop/pop.test.js
@@ -16,25 +16,41 @@ describe('Given the function pop', () => {
     test('should return a message Error ', () => {
       //Arrange
       let undefArray;
-      //const returnedValue = [];
+
       //Act
-      //const result2 = ;
+
       //Expect
       expect(() => pop(undefArray)).toThrow(TypeError);
     });
   });
 
-  describe('When receive an array', () => {
+  describe('When receive an array with Strings or Numbers', () => {
     test('should delete the last item and return it ', () => {
       //Arrange
       const array = ['a', 'b', 'c', 'd', 'e'];
-      const lastItem = ['e'];
+      const lastItem = 'e';
       //Act
       const result = pop(array);
       //Expect
       expect(result).toStrictEqual(lastItem);
     });
   });
+
+  describe('When receive an array with Strings or Numbers', () => {
+    test('should delete the last item modifying the array length', () => {
+      //Arrange
+      const array = ['a', 'b', 'c', 'd', 'e'];
+      const changedArray = ['a', 'b', 'c', 'd'];
+      //Act
+      const usingPopFunction = pop(array);
+      const arrayResult= console.log(array)
+
+      //Expect
+      expect(arrayResult)).toStrictEqual(changedArray);
+    });
+  });
+
+
 
   describe('When receive a String', () => {
     test('should throw a message error ', () => {
@@ -58,6 +74,42 @@ describe('Given the function pop', () => {
       expect(() => pop(numberVariable)).toThrow(
         TypeError("Can't use pop on a number")
       );
+    });
+  });
+
+  describe('When receive an array with one value which is the primitive NaN', () => {
+    test('should delete the item and return it ', () => {
+      //Arrange
+      const array = [NaN];
+      const returnedValue = NaN;
+      //Act
+      const result = pop(array);
+      //Expect
+      expect(result).toStrictEqual(returnedValue);
+    });
+  });
+
+  describe('When receive an array with Numbers and the last item is the primitive NaN', () => {
+    test('should delete the item and return it ', () => {
+      //Arrange
+      const array = [1, 2, 3, 4, NaN];
+      const returnedValue = NaN;
+      //Act
+      const result = pop(array);
+      //Expect
+      expect(result).toStrictEqual(returnedValue);
+    });
+  });
+
+  describe('When receive an array with Strings and the last item is the primitive NaN', () => {
+    test('should delete the item and return it ', () => {
+      //Arrange
+      const array = ['a', 'b', 'c', NaN];
+      const returnedValue = NaN;
+      //Act
+      const result = pop(array);
+      //Expect
+      expect(result).toStrictEqual(returnedValue);
     });
   });
 });
